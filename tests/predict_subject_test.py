600e03a43087377217baf79b7f8a623c6e3be266
Robert Meissner <robert.meissner@codecentric.de>
2022-05-24 09:16:37 +0200

    Adding classification
    Adding test data

--- tests/predict_subject_test.py
+++ tests/predict_subject_test.py
@@ -4,9 +4,9 @@ from src.app.classification.predict import Prediction
 def test_predict_subject():
     dummy_text = "Der Satz des Pythagoras lautet: a^2 + b^2 = c^2."
 
-    model_file = "../src/data/wirlernenonline.oeh3.h5"
-    label_file = "../src/data/wirlernenonline.oeh3.npy"
-    tokenizer_file = "../src/data/wirlernenonline.oeh3.pickle"
+    model_file = "test_data/wirlernenonline.oeh3.h5"
+    label_file = "test_data/wirlernenonline.oeh3.npy"
+    tokenizer_file = "test_data/wirlernenonline.oeh3.pickle"
 
     prediction = Prediction(model_file, label_file, tokenizer_file)
     assert prediction.tokenizer is not None
